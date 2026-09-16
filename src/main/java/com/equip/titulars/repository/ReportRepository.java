73e0c5680cb5223bcb39e8325a67384e024c3bdf
ArantxaLeGon <20arantxaleal@gmail.com>
2026-09-16 17:04:16 -0500

    Puleza del código

--- src/main/java/com/equip/titulars/repository/ReportRepository.java
+++ src/main/java/com/equip/titulars/repository/ReportRepository.java
@@ -1,18 +1,12 @@
 package com.equip.titulars.repository;
 
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.equip.titulars.entity.Reports;
 import com.equip.titulars.entity.ReportId;
 
 public interface ReportRepository extends JpaRepository<Reports,ReportId>{
-    Optional<Reports> findById(ReportId id);
-
-    List<Reports> findAll();
-
     Long countByIdIdPlayer(Long idPlayer);
 
     List<Reports> findByIdIdPlayer(Long idPlayer);
