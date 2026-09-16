667044e02a1abc9269d82493a4a96d2036084a3c
ArantxaLeGon <20arantxaleal@gmail.com>
2026-09-16 17:02:16 -0500

    Puleza del código

--- src/main/java/com/equip/titulars/repository/ReportRepository.java
+++ src/main/java/com/equip/titulars/repository/ReportRepository.java
@@ -4,14 +4,10 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.stereotype.Repository;
 
 import com.equip.titulars.entity.Reports;
 import com.equip.titulars.entity.ReportId;
 
-
-
-// @Repository
 public interface ReportRepository extends JpaRepository<Reports,ReportId>{
     Optional<Reports> findById(ReportId id);
 
